1fcbaf821fdf7076fd0a2106343fc5573077fd02
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-25 16:57:18 +0800

    Predict the new result and visualising model

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pylab as pl
 
 dataset = pd.read_csv("Position_Salaries.csv")
 X = dataset.iloc[:, 1:-1].values
-y = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 
 
 # Training decision tree regression model
@@ -13,3 +14,17 @@ regressor = DecisionTreeRegressor(random_state=0)
 regressor.fit(X, y)
 
 # Predicting New Result
+reg_predict_test = regressor.predict([[6.5]])
+print(reg_predict_test)
+
+
+# Visualising the Decision Tree Regression model with high resolution
+# X_flatted = X.flatten()
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title("Truth Or Bluff (Decision Tree Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+pl.show()
