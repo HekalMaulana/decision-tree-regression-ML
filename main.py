3e01a88036e751033a6913e2c8056d9c70407e91
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-25 13:00:26 +0800

    Training decision tree regression model

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv("Position_Salaries.csv")
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, :-1].values
+
+
+# Training decision tree regression model
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X, y)
+
+# Predicting New Result
